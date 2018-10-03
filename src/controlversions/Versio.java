6064544dd96715eeadd7ee33d734f3b7e602b430
David <David@LAPTOP-66DVRHG7>
2018-10-03 15:50:31 +0200

    Creación del tercer método.

--- src/controlversions/Versio.java
+++ src/controlversions/Versio.java
@@ -17,8 +17,12 @@ public class Versio {
     public void metodo1() {
 
     }
-    
-        public void metodo2() {
+
+    public void metodo2() {
+
+    }
+
+    public void metodo3() {
 
     }
 
